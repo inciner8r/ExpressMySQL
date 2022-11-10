d05f9812317da10e6040c18df51634571829e39e
inciner8r <punarvpawade17@gmail.com>
2022-11-10 23:54:20 +0530

    kek

--- client/index.js
+++ client/index.js
@@ -1,139 +1,180 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('http://localhost:5000/getAll')
-    .then(response => response.json())
-    .then(data => loadHTMLTable(data['data']));
-    
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://localhost:5000/getAll")
+    .then((response) => response.json())
+    .then((data) => loadHTMLTable(data["data"]));
 });
 
-document.querySelector('table tbody').addEventListener('click', function(event) {
+document
+  .querySelector("table tbody")
+  .addEventListener("click", function (event) {
     if (event.target.className === "delete-row-btn") {
-        deleteRowById(event.target.dataset.id);
+      deleteRowById(event.target.dataset.id);
     }
     if (event.target.className === "edit-row-btn") {
-        handleEditRow(event.target.dataset.id);
+      handleEditRow(event.target.dataset.id);
     }
-});
+  });
 
-const updateBtn = document.querySelector('#update-row-btn');
-const searchBtn = document.querySelector('#search-btn');
+const updateBtn = document.querySelector("#update-row-btn");
+const searchBtn = document.querySelector("#search-btn");
 
-searchBtn.onclick = function() {
-    const searchValue = document.querySelector('#search-input').value;
+searchBtn.onclick = function () {
+  const searchValue = document.querySelector("#search-input").value;
 
-    fetch('http://localhost:5000/search/' + searchValue)
-    .then(response => response.json())
-    .then(data => loadHTMLTable(data['data']));
-}
+  fetch("http://localhost:5000/search/" + searchValue)
+    .then((response) => response.json())
+    .then((data) => loadHTMLTable(data["data"]));
+};
 
 function deleteRowById(id) {
-    fetch('http://localhost:5000/delete/' + id, {
-        method: 'DELETE'
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            location.reload();
-        }
+  fetch("http://localhost:5000/delete/" + id, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        location.reload();
+      }
     });
 }
 
 function handleEditRow(id) {
-    const updateSection = document.querySelector('#update-row');
-    updateSection.hidden = false;
-    document.querySelector('#update-name-input').dataset.id = id;
+  const updateSection = document.querySelector("#update-row");
+  updateSection.hidden = false;
+  document.querySelector("#update-name-input").dataset.id = id;
 }
 
-updateBtn.onclick = function() {
-    const updateNameInput = document.querySelector('#update-name-input');
+updateBtn.onclick = function () {
+  const updateNameInput = document.querySelector("#update-name-input");
+  const updateAddressInput = document.querySelector("#update-address-input");
+  const updateAgeInput = document.querySelector("#update-age-input");
+  const updateDobInput = document.querySelector("#update-dob-input");
+  const updateRollnoInput = document.querySelector("#update-rollno-input");
+  const updateBloodInput = document.querySelector("#update-blood-input");
 
+  console.log(updateNameInput);
 
-    console.log(updateNameInput);
+  fetch("http://localhost:5000/update", {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      id: updateNameInput.dataset.id,
+      name: updateNameInput.value,
+      address: updateAddressInput.value,
+      age: updateAgeInput.value,
+      dob: updateDobInput.value,
+      rollno: updateRollnoInput.value,
+      blood: updateBloodInput.value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        location.reload();
+      }
+    });
+};
 
-    fetch('http://localhost:5000/update', {
-        method: 'PATCH',
-        headers: {
-            'Content-type' : 'application/json'
-        },
-        body: JSON.stringify({
-            id: updateNameInput.dataset.id,
-            name: updateNameInput.value
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            location.reload();
-        }
-    })
-}
-
-
-const addBtn = document.querySelector('#add-name-btn');
+const addBtn = document.querySelector("#add-name-btn");
 
 addBtn.onclick = function () {
-    const nameInput = document.querySelector('#name-input');
-    const name = nameInput.value;
-    nameInput.value = "";
+  const nameInput = document.querySelector("#name-input");
+  const addressInput = document.querySelector("#address-input");
+  const ageInput = document.querySelector("#age-input");
+  const dobInput = document.querySelector("#dob-input");
+  const rollnoInput = document.querySelector("#rollno-input");
+  const bloodInput = document.querySelector("#blood-input");
 
-    fetch('http://localhost:5000/insert', {
-        headers: {
-            'Content-type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify({ name : name})
-    })
-    .then(response => response.json())
-    .then(data => insertRowIntoTable(data['data']));
-}
+  const name = nameInput.value;
+  const address = addressInput.value;
+  const age = ageInput.value;
+  const dob = dobInput.value;
+  const rollno = rollnoInput.value;
+  const blood = bloodInput.value;
+  nameInput.value = "";
+
+  fetch("http://localhost:5000/insert", {
+    headers: {
+      "Content-type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      name: name,
+      address: address,
+      age: age,
+      dob: dob,
+      rollno: rollno,
+      blood: blood,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => insertRowIntoTable(data["data"]));
+};
 
 function insertRowIntoTable(data) {
-    console.log(data);
-    const table = document.querySelector('table tbody');
-    const isTableData = table.querySelector('.no-data');
+  console.log(data);
+  const table = document.querySelector("table tbody");
+  const isTableData = table.querySelector(".no-data");
 
-    let tableHtml = "<tr>";
+  let tableHtml = "<tr>";
 
-    for (var key in data) {
-        if (data.hasOwnProperty(key)) {
-            if (key === 'dateAdded') {
-                data[key] = new Date(data[key]).toLocaleString();
-            }
-            tableHtml += `<td>${data[key]}</td>`;
-        }
+  for (var key in data) {
+    if (data.hasOwnProperty(key)) {
+      if (key === "dateAdded") {
+        data[key] = new Date(data[key]).toLocaleString();
+      }
+      tableHtml += `<td>${data[key]}</td>`;
     }
+  }
 
-    tableHtml += `<td><button class="delete-row-btn" data-id=${data.id}>Delete</td>`;
-    tableHtml += `<td><button class="edit-row-btn" data-id=${data.id}>Edit</td>`;
+  tableHtml += `<td><button class="delete-row-btn" data-id=${data.id}>Delete</td>`;
+  tableHtml += `<td><button class="edit-row-btn" data-id=${data.id}>Edit</td>`;
 
-    tableHtml += "</tr>";
+  tableHtml += "</tr>";
 
-    if (isTableData) {
-        table.innerHTML = tableHtml;
-    } else {
-        const newRow = table.insertRow();
-        newRow.innerHTML = tableHtml;
-    }
+  if (isTableData) {
+    table.innerHTML = tableHtml;
+  } else {
+    const newRow = table.insertRow();
+    newRow.innerHTML = tableHtml;
+  }
 }
 
 function loadHTMLTable(data) {
-    const table = document.querySelector('table tbody');
+  const table = document.querySelector("table tbody");
 
-    if (data.length === 0) {
-        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
-        return;
-    }
+  if (data.length === 0) {
+    table.innerHTML = "<tr><td class='no-data' colspan='6'>No Data</td></tr>";
+    return;
+  }
 
-    let tableHtml = "";
+  let tableHtml = "";
 
-    data.forEach(function ({id, name, date_added}) {
-        tableHtml += "<tr>";
-        tableHtml += `<td>${id}</td>`;
-        tableHtml += `<td>${name}</td>`;
-        tableHtml += `<td>${new Date(date_added).toLocaleString()}</td>`;
-        tableHtml += `<td><button class="delete-row-btn" data-id=${id}>Delete</td>`;
-        tableHtml += `<td><button class="edit-row-btn" data-id=${id}>Edit</td>`;
-        tableHtml += "</tr>";
-    });
+  data.forEach(function ({
+    id,
+    name,
+    address,
+    age,
+    dob,
+    rollno,
+    blood,
+    date_added,
+  }) {
+    tableHtml += "<tr>";
+    tableHtml += `<td>${id}</td>`;
+    tableHtml += `<td>${name}</td>`;
+    tableHtml += `<td>${address}</td>`;
+    tableHtml += `<td>${age}</td>`;
+    tableHtml += `<td>${dob}</td>`;
+    tableHtml += `<td>${rollno}</td>`;
+    tableHtml += `<td>${blood}</td>`;
+    tableHtml += `<td>${new Date(date_added).toLocaleString()}</td>`;
+    tableHtml += `<td><button class="delete-row-btn" data-id=${id}>Delete</td>`;
+    tableHtml += `<td><button class="edit-row-btn" data-id=${id}>Edit</td>`;
+    tableHtml += "</tr>";
+  });
 
-    table.innerHTML = tableHtml;
+  table.innerHTML = tableHtml;
 }
--- server/dbService.js
+++ server/dbService.js
@@ -1,122 +1,135 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv');
+const mysql = require("mysql");
+const dotenv = require("dotenv");
 let instance = null;
 dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.DB_PORT
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT,
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.log(err.message);
-    }
-    // console.log('db ' + connection.state);
+  if (err) {
+    console.log(err.message);
+  }
+  // console.log('db ' + connection.state);
 });
 
-
 class DbService {
-    static getDbServiceInstance() {
-        return instance ? instance : new DbService();
+  static getDbServiceInstance() {
+    return instance ? instance : new DbService();
+  }
+
+  async getAllData() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM user;";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      // console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async getAllData() {
-        try {
-            const response = await new Promise((resolve, reject) => {
-                const query = "SELECT * FROM names;";
+  async insertNewName(name, address, age, dob, rollno, blood) {
+    try {
+      const dateAdded = new Date();
+      const insertId = await new Promise((resolve, reject) => {
+        const query =
+          "INSERT INTO user (name, address, age, dob, rollno, blood, date_added) VALUES (?,?,?,?,?,?,?);";
 
-                connection.query(query, (err, results) => {
-                    if (err) reject(new Error(err.message));
-                    resolve(results);
-                })
-            });
-            // console.log(response);
-            return response;
-        } catch (error) {
-            console.log(error);
-        }
+        connection.query(
+          query,
+          [name, address, age, dob, rollno, blood, dateAdded],
+          (err, result) => {
+            if (err) reject(new Error(err.message));
+            resolve(result.insertId);
+          }
+        );
+      });
+      return {
+        id: insertId,
+        name: name,
+        address: address,
+        age: age,
+        dob: dob,
+        rollno: rollno,
+        blood: blood,
+        dateAdded: dateAdded,
+      };
+    } catch (error) {
+      console.log(error);
     }
+  }
 
+  async deleteRowById(id) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "DELETE FROM user WHERE id = ?";
 
-    async insertNewName(name) {
-        try {
-            const dateAdded = new Date();
-            const insertId = await new Promise((resolve, reject) => {
-                const query = "INSERT INTO names (name, date_added) VALUES (?,?);";
+        connection.query(query, [id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result.affectedRows);
+        });
+      });
 
-                connection.query(query, [name, dateAdded] , (err, result) => {
-                    if (err) reject(new Error(err.message));
-                    resolve(result.insertId);
-                })
-            });
-            return {
-                id : insertId,
-                name : name,
-                dateAdded : dateAdded
-            };
-        } catch (error) {
-            console.log(error);
-        }
+      return response === 1 ? true : false;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
+  }
 
-    async deleteRowById(id) {
-        try {
-            id = parseInt(id, 10); 
-            const response = await new Promise((resolve, reject) => {
-                const query = "DELETE FROM names WHERE id = ?";
-    
-                connection.query(query, [id] , (err, result) => {
-                    if (err) reject(new Error(err.message));
-                    resolve(result.affectedRows);
-                })
-            });
-    
-            return response === 1 ? true : false;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
+  async updateNameById(id, name, address, age, dob, rollno, blood) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query =
+          "UPDATE user SET name = ?, address = ?, age = ? , dob = ? , rollno = ? , blood = ? WHERE id = ?";
+
+        connection.query(
+          query,
+          [name, address, age, dob, rollno, blood, id],
+          (err, result) => {
+            if (err) reject(new Error(err.message));
+            resolve(result.affectedRows);
+          }
+        );
+      });
+
+      return response === 1 ? true : false;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
+  }
 
-    async updateNameById(id, name) {
-        try {
-            id = parseInt(id, 10); 
-            const response = await new Promise((resolve, reject) => {
-                const query = "UPDATE names SET name = ? WHERE id = ?";
-    
-                connection.query(query, [name, id] , (err, result) => {
-                    if (err) reject(new Error(err.message));
-                    resolve(result.affectedRows);
-                })
-            });
-    
-            return response === 1 ? true : false;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
+  async searchByName(name) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM user WHERE name = ?;";
+
+        connection.query(query, [name], (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+
+      return response;
+    } catch (error) {
+      console.log(error);
     }
-
-    async searchByName(name) {
-        try {
-            const response = await new Promise((resolve, reject) => {
-                const query = "SELECT * FROM names WHERE name = ?;";
-
-                connection.query(query, [name], (err, results) => {
-                    if (err) reject(new Error(err.message));
-                    resolve(results);
-                })
-            });
-
-            return response;
-        } catch (error) {
-            console.log(error);
-        }
-    }
+  }
 }
 
 module.exports = DbService;
